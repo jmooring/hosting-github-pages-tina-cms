735243adb217ecc7f8acdec7da12314f828f8c92
Joe Mooring <joe.mooring@veriphor.com>
2024-04-25 15:45:10 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Tina CMS Test
+
+This is a test of hosting a Hugo site on GitHub Pages with TinaCMS.
+
+See <https://jmooring.github.io/hosting-github-pages-tina-cms/>
+
+## Notes
+
+Documentation:
+
+- <https://tina.io/docs/frameworks/hugo/>
+- <https://github.com/tinacms/tina-hugo-starter>
+- <https://tina.io/docs/schema/>
+- <https://tina.io/docs/tina-cloud>
+- <https://github.com/tinacms/tinacms/discussions>
+
+To start TinaCMS:
+
+```text
+npx tinacms dev -c "hugo server -D -p 1313"
+```
+
+Then navigate to:
+
+```text
+http://localhost:1313/hosting-github-pages-tina-cms/admin/
+```
---
+++ tina/config.ts
@@ -0,0 +1,94 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
+export default defineConfig({
+  branch,
+
+  // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+    basePath: "hosting-github-pages-tina-cms",
+  },
+  media: {
+    tina: {
+      mediaRoot: "media",
+      publicFolder: "static",
+    },
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        ui: {
+          filename: {
+            // if disabled, the editor can not edit the filename
+            readonly: true,
+            // Example of using a custom slugify function
+            slugify: (values) => {
+              // Values is an object containing all the values of the form. In this case it is {title?: string, topic?: string}
+              return `${values?.title
+                ?.toLowerCase()
+                .replace(/ /g, '-')}`
+            },
+          },
+        },
+        defaultItem: () => {
+          return {
+            // When a new post is created the title field will be set to "New post"
+            title: 'New Post',
+            date: new Date(),
+            draft: false,
+          }
+        },
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "datetime",
+            name: "date",
+            label: "Date",
+            required: true,
+          },
+          {
+            type: "boolean",
+            name: "draft",
+            label: "Draft",
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+  search: {
+    tina: {
+      stopwordLanguages: ['eng'],
+    },
+    indexBatchSize: 100,
+    maxSearchIndexFieldLength: 100,
+  }
+});
